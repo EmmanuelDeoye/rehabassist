4707a74d006674c956b6410b35c6366941e94de1
EmmanuelDeoye <kingemmanueldeoye@gmail.com>
2026-05-18 05:37:21 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,19 @@
+const CACHE_NAME = 'rehablix-v1';
+
 self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(
-      (resp) => resp || fetch(event.request)
-    )
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
   );
 });
